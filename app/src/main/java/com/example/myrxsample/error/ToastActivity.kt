bdf3d72f6621bfa7d33cdc2c1f79d9b2b51c960b
JasonChien <jasonchien@kkstream.com>
2020-03-24 06:28:31 +0800

    Lesson 1: error toast.

---
+++ app/src/main/java/com/example/myrxsample/error/ToastActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myrxsample.error
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myrxsample.R
+
+class ToastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toast)
+    }
+}
